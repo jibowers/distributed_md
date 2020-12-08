6c8fa56995889d737faeedc5e3fe69620d08917b
Julia Bowers <jibowers@users.noreply.github.com>
2020-12-07 18:26:09 -0600

    updated lambda and added gitignore

--- md_worker_lambda.py
+++ md_worker_lambda.py
@@ -30,46 +30,57 @@ def lambda_handler(event, context):
         #logger.info("Positions: {}\nCharges: {}\nVelocities: {}\nEpsilons: {}\nRmins: {}".format(position_array, charge_array, velocity_array, eps, rmins))
         logger.info("Epsilons: {}".format(eps))
         logger.info("## Got all the arguments passed to this lambda")
-        new_positions = np.empty(3)
+        
+        new_positions = []
         particle_mass = 0.00000001
+        num_particles = len(position_array)
+        logger.info("## Num particles: {}".format(num_particles))
+        logger.info("Length of charge_array: {}".format(len(charge_array)))
+        logger.info("Length of velocity_array: {}".format(len(velocity_array)))
+        logger.info("Length of eps: {}".format(len(eps)))
+        logger.info("Length of rmins: {}".format(len(rmins)))
         # calculate Eelec and Evdw
         const = 332.1*4.184
-        for i in range(start, end):
+        for i in range(start, end+1):
+            logger.info("## Atom {}".format(i))
             eelec = 0
             evdw = 0
             force_array = []
             a = position_array[i]
-            for j in range (start, end):
+            for j in range(num_particles):
                 if i != j:
                     #calculate distance
                     b = position_array[j]
-                    logger.info("## Calculating energy terms between atom {} (at {}) and atom {} (at {})".format(i, a, j, b))
+                    #logger.info("## Atom {} (looking at atom {} now)".format(i, j))
                     dist = np.linalg.norm(a-b)
-                    logger.info("Distance is {}".format(dist))
+                    # logger.info("Distance is {}".format(dist))
                     
-                    logger.info("Calculating eelec using charges and distance")
-                    eelec = const*charge_array[i]*charge_array[j]/dist
-                    logger.info("Calculating evds using charges and distance")
+                    # Calculating magnitude of force
+                    #logger.info("Calculating derivative of eelec using charges and distance")
+                    delec = -1*const*charge_array[i]*charge_array[j]/(dist**2)
+                    #logger.info("Calculating derivative of evdw using distance and parameters")
                     eij = math.sqrt(eps[i]*eps[j])*4.184
-                    logger.info("Determined eij")
+                    #logger.info("Determined eij")
                     rij = rmins[i] + rmins[j]
-                    logger.info("Determined rij")
-                    evdw = eij*( (rij/dist)**12 - 2 * (rij/dist)**6)
-                    logger.info("## Distance: {}, Eelec: {}, Evdw: {}".format(dist, eelec, evdw))
+                    #logger.info("Determined rij")
+                    #evdw = eij*( (rij/dist)**12 - 2 * (rij/dist)**6)
+                    dvdw = eij * (-12*((rij/dist)**12)/dist + 6*((rij/dist)**6)/dist)
+                    #logger.info("## Distance: {}, Eelec: {}, Evdw: {}".format(dist, eelec, evdw))
     
-                    unit_vector = a-b/dist
-                    force_vector = unit_vector*(eelec+evdw)
-                    logger.info("## Force vector is {}".format(force_vector))
+                    # Calculating the direction of the force produced by both elec and vdw
+                    unit_vector = (a-b)/dist
+                    force_vector = unit_vector*(delec+dvdw)
+                    #logger.info("## Force vector is {}".format(force_vector))
                     force_array.append(force_vector)
 
             #look at force array to determine ultimate force on particle (and thus acceleration and new velocity)
-            logger.info("Determine acceleration from {}".format(force_array))
+            #logger.info("Determine acceleration from {}".format(force_array))
             pre_mass_acceleration = [sum(x) for x in zip(*force_array)] #/particle_mass
             acceleration_w_time = [m/particle_mass*timestep  for m in pre_mass_acceleration]
-            logger.info("## Acceleration for particle {} is {}".format(i, acceleration_w_time))
+            #logger.info("## Acceleration for particle {} is {}".format(i, acceleration_w_time))
 
             velocity = [sum(x) for x in zip(velocity_array[i-start], acceleration_w_time)]
-            logger.info("## Velocity for particle {} is {}".format(i, velocity))
+            #logger.info("## Velocity for particle {} is {}".format(i, velocity))
             velocity_array[i-start] = velocity
 
             velocity_w_time = [m*timestep for m in velocity]
@@ -77,15 +88,19 @@ def lambda_handler(event, context):
             # to get wrapping to other side of box if particle leaves the box
             for d in range(len(position)):
                 if position[d] > bounds:
-                    position[d]  = position[d] - bounds
+                    position[d]  = position[d]%bounds
                 elif position[d] < 0:
-                    position[d] = bounds + position[d]
+                    position[d] = bounds + -position[d]%bounds
             logger.info("## Particle {} went from {} to {}".format(i, a, position))
-            new_positions = np.append(new_positions, position)
-
+            new_positions.append(position)
+            
+        logger.info("## Length of position array: {}, Contents: {}".format(len(position_array), position_array))
+        
         return {
             'statusCode': 200,
-            'positions': new_positions.tolist(),
+            'start_index': start,
+            'end_index': end,
+            'positions': new_positions,
             'velocities': velocity_array.tolist()
         }
     except Exception as e:
